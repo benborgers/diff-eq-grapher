582c539c27b4697e588897ebbc177e4ec0906f2d
Ben Borgers <borgersbenjamin@gmail.com>
2024-01-13 21:05:28 -0500

    Refactor Button

--- resources/js/Components/Button.tsx
+++ resources/js/Components/Button.tsx
@@ -1,33 +1,41 @@
 import { twMerge } from "tailwind-merge";
 
-interface CustomButtonProps extends React.HTMLProps<HTMLButtonElement> {
-    children: React.ReactNode;
-    as?: "button";
-    type?: "button" | "submit";
-    className?: string;
-    color?: "default" | "emphasis";
-}
-
 export default function Button({
     as = "button",
     children,
     type = "button",
     className = "",
     color = "default",
-    ...attributes
-}: CustomButtonProps) {
-    const Element = as;
-    return (
-        <Element
-            className={twMerge(
-                "px-3 py-1.5 border-2 border-black rounded-full block w-max font-semibold bg-white hover:bg-black hover:text-white transition-colors duration-150",
-                color === "emphasis" && "bg-red-500 text-white",
-                className
-            )}
-            type={type}
-            {...attributes}
-        >
-            {children}
-        </Element>
+    href,
+    download,
+}: {
+    children: React.ReactNode;
+    as?: "button" | "a";
+    type?: "button" | "submit";
+    className?: string;
+    color?: "default" | "emphasis";
+    href?: string;
+    download?: boolean;
+}) {
+    const _className = twMerge(
+        "px-3 py-1.5 border-2 border-black rounded-full block w-max font-semibold bg-white hover:bg-black hover:text-white transition-colors duration-150",
+        color === "emphasis" && "bg-red-500 text-white",
+        className
     );
+
+    if (as === "a") {
+        return (
+            <a href={href} download={download} className={_className}>
+                {children}
+            </a>
+        );
+    }
+
+    if (as === "button") {
+        return (
+            <button type={type} className={_className}>
+                {children}
+            </button>
+        );
+    }
 }
