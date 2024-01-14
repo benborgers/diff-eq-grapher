4b053234a7f233bb729d2738ca08cf757e4fa1fb
Ben Borgers <borgersbenjamin@gmail.com>
2024-01-13 22:16:20 -0500

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>Differential Equation Grapher</title>
+        <link rel="icon" href="/favicon.png" />
 
         @routes
         @viteReactRefresh
