544af796ca65757a4f4ff802966533a2942f3886
Ben Borgers <borgersbenjamin@gmail.com>
2024-01-18 23:22:29 -0500

    Make π character work

--- resources/python/graph.py
+++ resources/python/graph.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from sympy import symbols, lambdify, E
+from sympy import symbols, lambdify, E, pi
 from sympy.parsing.sympy_parser import parse_expr
 import numpy as np
 from scipy.integrate import odeint
@@ -14,7 +14,7 @@ y_symbol, t_symbol = symbols('y t')
 
 for equation in payload['equations']:
   # Usually the mathematical constant e has to be written as E, but use allow_dict to recognize e as well.
-  clean_expr = parse_expr(equation['value'], local_dict={'e': E}, transformations='all')
+  clean_expr = parse_expr(equation['value'], local_dict={'e': E, 'π': pi}, transformations='all')
   func = lambdify((y_symbol, t_symbol), clean_expr, 'numpy')
   initial_condition = float(equation['initialCondition'])
   solution = odeint(func, initial_condition, t)
