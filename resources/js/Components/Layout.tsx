6addbefdf30808a62511c6a2c0f2218f117f5dcb
Ben Borgers <borgersbenjamin@gmail.com>
2024-02-01 22:53:26 -0500

    Layout tweaks

--- resources/js/Components/Layout.tsx
+++ resources/js/Components/Layout.tsx
@@ -6,20 +6,20 @@ const Layout = ({
   right: React.ReactNode;
 }) => {
   return (
-    <div className="p-4 sm:p-8 sm:pb-4 sm:grid sm:min-h-screen sm:grid-rows-[max-content,1fr,max-content] sm:grid-cols-[625px,1fr]">
+    <div className="p-4 md:p-8 md:pb-4 md:grid md:min-h-screen md:grid-rows-[max-content,1fr,max-content] md:grid-cols-[450px,1fr] lg:grid-cols-[625px,1fr]">
       <div className="col-span-2">
         <h1 className="text-2xl font-bold text-gray-900">
           Differential Equation Grapher
         </h1>
       </div>
 
-      <div className="min-w-0 pt-8 sm:border-r-2 sm:border-black/10 sm:border-dashed sm:pr-10">
+      <div className="min-w-0 pt-8 md:border-r-2 md:border-black/10 md:border-dashed md:pr-10">
         {left}
       </div>
 
-      <div className="min-w-0 pt-16 sm:pt-0 sm:pl-10">{right}</div>
+      <div className="min-w-0 pt-16 md:pt-0 md:pl-10">{right}</div>
 
-      <footer className="mt-20 sm:mt-6 col-span-2 border-t-2 border-black/10 border-dashed pt-3">
+      <footer className="mt-20 md:mt-6 col-span-2 border-t-2 border-black/10 border-dashed pt-3">
         <p className="text-sm text-black/40 font-medium">
           Built by{" "}
           <a href="https://ben.page" className="underline">
